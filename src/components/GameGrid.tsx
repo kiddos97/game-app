8293f50302f7ee1aeab77264eb0c22acfff03269
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-16 16:05:03 -0600

    updating game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing="20px">
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
