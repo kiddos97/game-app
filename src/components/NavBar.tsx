b9406e3fe58602718371a666d38aa6a7dbd561a2
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-16 11:54:53 -0600

    updating

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.webp";
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={logo} />
+      <Image src={logo} boxSize="60px" />
     </HStack>
   );
 };
